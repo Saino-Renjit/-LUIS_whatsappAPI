4e00339729bd3da87512248d76109e6636355a95
Saino-Renjit <sainorenjit20@gmail.com>
2022-12-24 22:57:53 +0530

    Second Commit

--- helperfunction/waSendMessage.py
+++ helperfunction/waSendMessage.py
@@ -5,12 +5,8 @@ from twilio.rest import Client
 #from dotenv import load_dotenv
 #load_dotenv()
 
-#ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
-#AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
-#FROM = os.environ.get('FROM')
-
 ACCOUNT_SID='ACb68c762e025a1c97b1afd67a5f85f3cb'
-AUTH_TOKEN='e1a482366aab8fb216d5f8bcd91da494'
+AUTH_TOKEN='024f5f50477a7a45243463c098f727fd'
 FROM='whatsapp:+14155238886'
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
--- processWebhook.py
+++ processWebhook.py
@@ -7,11 +7,6 @@ from flask import send_from_directory, request
 
 app = flask.Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
-
 @app.route('/')
 @app.route('/home')
 def home():
@@ -21,10 +16,10 @@ from helperfunction.waSendMessage import sendMessage
 
 @app.route('/whatsapp', methods=['GET', 'POST'])
 def whatsapp():
-    print(request.get_data())
+    
     query= "https://zxcvbnm.cognitiveservices.azure.com/luis/prediction/v3.0/apps/5a60d162-2a8b-4c82-984c-3b38870b3c5a/slots/production/predict?verbose=true&show-all-intents=true&log=true&subscription-key=4ca9b36a470e404f83a9c3d065794eb1&query="
-    message1 = request.form['Body']
-    message=query+message1
+    whatsapp_message = request.form['Body']
+    message=query+whatsapp_message
 
     response_API = requests.get(message)
 
@@ -32,9 +27,13 @@ def whatsapp():
     parse_json =json.loads(data)
     senderId = request.form['From'].split('+')[1]
     try:
-        output=parse_json["prediction"]['entities']["RestaurantReservation.Time"]
+        result=parse_json['prediction']['entities']['RestaurantReservation.Time']
+        s1= str(result)
+        
+        s2='Your booking schedule is updated to :'
+        output="".join([s2, s1])
     except:
-        output='Please specify the new time '
+        output='Please specify the time for which the reservation is required.'
     
     
     res = sendMessage(senderId=senderId, message=output)
